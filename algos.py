b47eb21682020380715f4848128355fdc229d7bb
Aisalkyn Abdyvalieva <salkyna@rocketmail.com>
2022-09-30 10:32:55 +0100

    Create algos.py

---
+++ algos.py
@@ -0,0 +1,25 @@
+from algorithms import *
+from algorithms.binaryinsertionSort import binaryinsertionSort
+from algorithms.timSort     import timSort
+
+algorithmsDict = {'insertion'       : insertionSort,
+                  'bubble'          : bubbleSort,
+                  'selection'       : selectionSort,
+                  'merge'           : mergeSort,
+                  'quick'           : quickSort,
+                  'counting'        : countingSort,
+                  'cocktail'        : cocktailSort,
+                  'cycle'           : cycleSort,
+                  'bogo'            : bogoSort,
+                  'heap'            : heapSort,
+                  'radix'           : radixSort,
+   
+                  'comb'            : combSort,
+                  'bitonic'         : bitonicSort,
+                  'pancake'         : pancakeSort,
+                  'binary insertion': binaryinsertionSort,
+                  'bucket'          : bucketSort,
+                  'tim'             : timSort
+           
+          
+                }
